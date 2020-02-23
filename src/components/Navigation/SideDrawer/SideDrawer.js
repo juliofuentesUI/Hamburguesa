131ac1488d4daab7334aadce8c777e1cca8e5672
Julio Fuentes <juliofuentes408@gmail.com>
2020-02-23 12:37:02 -0800

    Implement responsive design by changing base classes to mobile first

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/BackDrop/Backdrop';
+
+const sideDrawer = (props) => {
+  // ... conditionally attach css classes.
+  return (
+    <React.Fragment>
+      <Backdrop show/>
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default sideDrawer;
