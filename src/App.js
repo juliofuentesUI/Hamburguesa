9a29f5b90a5ec6b7a953e197836fe683a50edc71
Julio Fuentes <juliofuentes408@gmail.com>
2020-03-02 14:17:15 -0800

    Begin Routing to CheckoutSummary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/layout/Layout';
 import BurgerBuilder from './containers/burger-builder/BurgerBuilder';
+import Checkout  from './containers/Checkout/Checkout';
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder/>
+          <Checkout />
         </Layout>
       </div>
     )
